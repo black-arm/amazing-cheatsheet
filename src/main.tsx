e37d9717d82d015e69af6e76ce13d9a11f82aefa
Antonio Basile <antoniobasile2@eng.it>
2024-10-07 22:51:12 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
