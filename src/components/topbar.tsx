1dba8bcfd15ea662a458280897046336447a2abb
Antonio Basile <a.basile@innovationengineering.eu>
2024-10-30 12:25:23 +0100

    feat: sidebar responsive

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -2,13 +2,18 @@ import githubSvg from '@/assets/icons/github.svg';
 import sunSvg from '@/assets/icons/sun.svg';
 import moonSvg from '@/assets/icons/moon.svg';
 import githubMoonSvg from '@/assets/icons/github-moon.svg';
-
+import barsSvg from '@/assets/icons/bars.svg';
+import barsDarkSvg from '@/assets/icons/bars-dark.svg';
+import closeSvg from '@/assets/icons/close.svg';
+import closeDarkSvg from '@/assets/icons/close-dark.svg';
 import { Link } from '@tanstack/react-router';
 import { useTheme } from './theme-provider';
+import { useState } from 'react';
 
-export const Topbar = () => {
+export const Topbar = ({ onToggleSidebar }: { onToggleSidebar: (value: boolean) => void }) => {
 
     const { theme, setTheme } = useTheme()
+    const [toggleBars, setToggleBars] = useState(false);
 
     const changeTheme = () => {
         if(theme === 'dark'){
@@ -18,14 +23,30 @@ export const Topbar = () => {
         }
     }
 
+    const onSetToggleBars = () => {
+        setToggleBars(!toggleBars)
+        onToggleSidebar(!toggleBars);
+    }
+
     return <header className="sticky top-0 z-50 backdrop-blur-sm bg-background/80 drop-shadow-md">
         <nav className="p-6 flex justify-between items-center">
-            <Link to='/'>
+            <div className='flex flex-row gap-4 items-center'>
+                {
+                    !toggleBars ? <img alt="bars" src={theme === 'light' ? barsSvg: barsDarkSvg} 
+                                    className='w-5 h-5 block lg:hidden cursor-pointer' onClick={onSetToggleBars} /> :
+                                 <img alt="close" src={theme === 'light' ? closeSvg: closeDarkSvg} 
+                                    className='w-5 h-5 block lg:hidden cursor-pointer' onClick={onSetToggleBars}/> 
+                }
+            <Link to='/' onClick={() => {
+                setToggleBars(false)
+                onToggleSidebar(false)
+            }}>
                 <h3 className="text-2xl
                     title-color">
                     Amazing Cheatsheet
                 </h3>
             </Link>
+            </div>
             <div className='flex gap-2'>
                 <a role='link' href="https://github.com/black-arm/amazing-cheatsheet" target='_blank'>
                     <img  src={theme === 'light' ? githubSvg : githubMoonSvg} alt='github logo' className='w-5 h-5'/>
