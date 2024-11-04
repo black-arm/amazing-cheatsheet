85b939b534d861fb89a90a01182c9b1f013556d4
Antonio Basile <a.basile@innovationengineering.eu>
2024-11-04 16:05:29 +0100

    fix: update tech json files and $tech.lazy component

--- src/routes/cheatsheet/$tech.tsx
+++ src/routes/cheatsheet/$tech.tsx
@@ -42,13 +42,13 @@ function CheatsheetPage() {
 
   return (
     <div className="flex flex-col justify-center p-4">
-      <h1 className="text-3xl title-color p-3 text-center uppercase">{tech}</h1>
+      <h1 className="text-3xl xl:text-4xl title-color p-3 text-center uppercase">{tech}</h1>
       <div className='py-2'>
         <Label className='title-color' htmlFor="search">Search Text</Label>
         <Input type="text" id="Search" placeholder="Search Reference" onChange={onChange} />
       </div>
       <div className="bg-gradient-to-br from-cyan-500 to-blue-600 rounded-md">
-        <div className="bg-background m-px p-1 rounded-md grid lg:grid-cols-3 grid-cols-2 gap-1">
+        <div className="bg-background m-px p-1 rounded-md grid lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-cols-2 gap-1">
           {references?.map((item, index) => (
             <div key={index} className="w-full truncate">
               <Tooltip key={index}>
@@ -56,7 +56,7 @@ function CheatsheetPage() {
                   <a
                     href={item.href}
                     target="_blank"
-                    className="p2 md:p-1 truncate font-thin hover:title-color"
+                    className="p2 md:p-1 xl:p-2 2xl:p-3 truncate font-thin hover:title-color"
                   >
                     {item.text}
                   </a>
